ff0392cb4716195367975187ca407a3e284d3caa
Jeremy Fuentes <jeremyfuentes201@gmail.com>
2024-10-20 12:23:57 -0600

    Guia VII

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e762b386fc3bf1c4d7f35b6ce5e22d04eb47f721")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96526ab236779d1d2386221e847053928f794bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
